862bd97bf92c07bcde5d91ae225a03abbffdc173
MingFang914 <phuongvnmp@gmail.com>
2026-03-26 15:10:22 +0700

    first commit

---
+++ src/main/java/com/example/DuAnJava/DuAnJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.DuAnJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuAnJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuAnJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DuAnJavaApplicationTests/DuAnJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DuAnJavaApplicationTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuAnJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
